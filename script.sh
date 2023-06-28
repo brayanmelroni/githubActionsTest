f04bf79526eb4cb06bba9b1ff407788aeba5ef96
Brayan Melroni Gajamuthu Lankathilaka Chandrappulige <brayanmelroni1@gmail.com>
2023-06-28 09:01:00 +0100

    M1

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ delete_connector () {
     CONNECT_REST_BASIC_AUTH_PASSWORD=$4
     CONNECTOR_NAME=`echo $file | rev |  cut -d/ -f1 | rev | cut -d. -f1`
     DELETE_URL=`echo ${BASE_URL}/connectors/${CONNECTOR_NAME}`
-    response=$(curl --write-out '%{http_code}' --silent --output response.txt -u ${{ vars.CONNECT_REST_BASIC_AUTH_USER }}:${{ secrets.CONNECT_REST_BASIC_AUTH_PASSWORD }} -s -X DELETE $DELETE_URL)
+    response=$(curl --write-out '%{http_code}' --silent --output response.txt -u ${CONNECT_REST_BASIC_AUTH_USER}:${CONNECT_REST_BASIC_AUTH_PASSWORD} -s -X DELETE $DELETE_URL)
     if [ $response == 204 ]
     then
         echo "Deleted: $CONNECTOR_NAME"
