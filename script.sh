4f1592fafe6c82941e88701ed1b82fb513a2117f
Brayan Melroni Gajamuthu Lankathilaka Chandrappulige <brayanmelroni1@gmail.com>
2023-07-01 22:10:34 +0100

    fanta

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 validate_new_files(){
   created_files=$1
+  starts_with=$2
   echo $created_files
   for file in $created_files; do
     echo $file
-    if  [[ $file == connectors* ]] && [[ $file != *.json ]] ;
+    echo $starts_with
+    if  [[ $file == ${starts_with}* ]] && [[ $file != *.json ]] ;
     then
       echo "ERROR: new file name: $file ... should not contain space(s)"
       #exit 1
